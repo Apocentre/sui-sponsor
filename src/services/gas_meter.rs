20ed2ac6343deb47a81e5d8ee1ecae923178c20b
ppoliani <ppoliani@gmail.com>
2023-06-02 16:02:53 +0300

    feat: implement total_gas_used and total_gas_used_upper_bound

--- src/services/gas_meter.rs
+++ src/services/gas_meter.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
-use eyre::Result;
-use sui_sdk::SuiClient;
+use eyre::{Result, Report};
+use sui_sdk::{SuiClient, rpc_types::SuiTransactionBlockEffects};
+use sui_types::{transaction::TransactionData, gas::GasCostSummary};
 
 pub struct GasMeter {
   api: Arc<SuiClient>,
@@ -19,8 +20,34 @@ impl GasMeter {
     Ok(gas)
   }
 
-  pub fn gas_budget(&self) -> u64 {
-    // TODO: Compute gas estimation
-    0
+  pub async fn gas_budget(&self, tx_data: TransactionData) -> Result<u64> {
+    let tx_effects = self.api.read_api()
+    .dry_run_transaction_block(tx_data)
+    .await?;
+
+    Ok(0)
+  }
+
+  pub fn total_gas_used(tx_block_effects: SuiTransactionBlockEffects) -> Result<u64> {
+    let gas_summary = Self::gas_summary(tx_block_effects);
+    let gas_used = gas_summary.computation_cost
+    + gas_summary.storage_cost
+    - gas_summary.storage_rebate;
+
+    Ok(gas_used)
+  }
+
+  pub fn total_gas_used_upper_bound(tx_block_effects: SuiTransactionBlockEffects) -> Result<u64> {
+    let gas_summary = Self::gas_summary(tx_block_effects);
+    // Note for the upper bound we don't subtract`storage_rebate`. This is similar to how the TS SDK computes this value
+    let gas_upper_boud = gas_summary.computation_cost + gas_summary.storage_cost;
+    
+    Ok(gas_upper_boud)
+  }
+
+  fn gas_summary(tx_block_effects: SuiTransactionBlockEffects) -> GasCostSummary {
+    match tx_block_effects {
+      SuiTransactionBlockEffects::V1(effects_v1) => effects_v1.gas_used,
+    }
   }
 }
--- src/services/sponsor.rs
+++ src/services/sponsor.rs
@@ -1,6 +1,6 @@
 use eyre::{Result};
 use sui_types::{
-  transaction::GasData, crypto::Signer,
+  transaction::{GasData, TransactionData}, crypto::Signer,
 };
 use crate::utils::config::KeyPair;
 use super::{
@@ -20,21 +20,21 @@ impl Sponsor {
     }
   }
 
-  async fn create_gas_data(&self) -> Result<GasData> {
+  async fn create_gas_data(&self, tx_data: TransactionData) -> Result<GasData> {
     let pubkey = &self.sponsor_keypair.public();
 
     let gas_data = GasData {
       payment: vec![get_gas_object()?],
       owner: pubkey.into(),
       price: self.gas_meter.gas_price().await?,
-      budget: self.gas_meter.gas_budget(),
+      budget: self.gas_meter.gas_budget(tx_data).await?,
     };
   
     Ok(gas_data) 
   }
 
-  pub async fn request_gas(&self) -> Result<String> {
-    let gas_data = self.create_gas_data().await?;
+  pub async fn request_gas(&self, tx_data: TransactionData) -> Result<String> {
+    let gas_data = self.create_gas_data(tx_data).await?;
     let sig = self.sponsor_keypair.sign(&bincode::serialize(&gas_data)?);
     let sig_str = serde_json::to_string(&sig)?;
     
