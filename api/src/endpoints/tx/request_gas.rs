66c1330ef06e1a555ece8c82fb716e248db32de7
Antonis Poulakis <antwnic4@gmail.com>
2023-07-10 13:02:14 +0300

    feat: pass transaction kind and sender in request gas endpoint

--- api/src/endpoints/tx/request_gas.rs
+++ api/src/endpoints/tx/request_gas.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
 use eyre::{eyre, Result};
-use sui_types::{transaction::{TransactionData, GasData}};
+use sui_types::{transaction::{TransactionKind, GasData}, base_types::SuiAddress};
 use crate::utils::error::Error;
 use sui_sponsor_common::{
   map_err,
@@ -11,6 +11,8 @@ use sui_sponsor_common::{
 #[derive(Deserialize)]
 pub struct Body {
   tx_data: String,
+  // TODO: this must be a signature which we can use to recover the sender address
+  sender: SuiAddress,
 }
 
 #[derive(Serialize)]
@@ -23,8 +25,8 @@ pub async fn exec(
   body: web::Json<Body>,
 ) -> Result<HttpResponse, Error> {
   let tx_data = map_err!(base64::decode(&body.tx_data))?;
-  let tx_data: TransactionData = map_err!(bcs::from_bytes(&tx_data))?;
-  let gas_data = store.sponsor.request_gas(tx_data).await?;
-  
+  let tx_data: TransactionKind = map_err!(bcs::from_bytes(&tx_data))?;
+  let gas_data = store.sponsor.request_gas(tx_data, body.sender).await?;
+
   Ok(HttpResponse::Ok().json(Response {gas_data}))
 }
